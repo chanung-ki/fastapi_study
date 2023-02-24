50cbbb0ce1c6d884d95c4d85a9641d2e62dc3725
chanung-ki <cksdnd7416@gmail.com>
2023-02-24 20:02:10 +0900

    path param , query String , Post 데이터 받는법

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
 import uvicorn
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl #=> pydantic 만의 데이터 타입
+
 
 app = FastAPI()
 
-@app.get("/")
-def hello():
-    return "Hello, World!"
+
+class User(BaseModel):
+    name: str
+    password: str
+    avatar_url: Optional[HttpUrl] = None
+
+
+@app.post("/users")
+def create_user(user: User):
+    return user
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
